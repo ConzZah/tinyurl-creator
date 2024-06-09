d19fa7ee809b69732a7d97db11ac684ab16a1ea4
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-09 18:25:53 +0200

    Update tinyurl-creator.sh to v1.1.1
    - saving is now optional, it will ask you whenever you want to save a result.

--- tinyurl-creator.sh
+++ tinyurl-creator.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-  #=============================================
+  #================================================
   # Project: TINYURL-CREATOR.sh
   # Author:  ConzZah / ©️ 2024
-  # Last Modification: 09.06.2024 / 15:30 [v1.1]
-  #=============================================
+  # Last Modification: 09.06.2024 / 18:24 [v1.1.1]
+  #================================================
 function main {
 clear; c1="============================="
 baseurl="https://tinyurl.com/api-create.php?url="
@@ -12,25 +12,45 @@ echo "ENTER URL"; echo ""; read raw_link
 curl -s -o x.txt "$baseurl$raw_link"; shortlink=$(<x.txt); rm x.txt
 echo ""; echo "$c1$c1"; echo ""; echo "shortlink:"; echo ""; echo "$shortlink"; echo ""; echo ""
 echo "original_url:"; echo ""; echo "$raw_link"; echo ""; echo "$c1$c1"
-echo ""; echo "SAVING RESULTS TO: $sav ..."; echo ""
+ask2save
+}
+# ask2save
+function ask2save {
+yes="SAVING RESULTS TO: $sav"
+no="YOU CHOSE NOT TO SAVE."
+echo ""; echo "DO YOU WANT TO SAVE THE RESULT TO $sav?"; echo ""
+echo "Y) YES"
+echo "N) NO"
+read ask2save
+case $ask2save in
+	y) echo ""; echo "$yes"; save;;
+	Y) echo ""; echo "$yes"; save;;
+	n) echo ""; echo "$no"; ask2repeat_main
+	N) echo ""; echo "$no"; ask2repeat_main
+	*) ask2save
+esac
+}
+# save
+function save {
 echo "$c1$c1">> $sav; echo "">> $sav; echo "shortlink:">> $sav; echo "">> $sav
 echo "$shortlink">> $sav; echo "">> $sav; echo "">> $sav
 echo "original_url:">> $sav; echo "">> $sav
 echo "$raw_link">> $sav; echo "">> $sav
-echo "DONE"; echo "" 
-repeat
+echo ""; echo "DONE"; echo "" 
+ask2repeat_main
 }
-function repeat {
+
+function ask2repeat_main {
 echo ""; echo "DO YOU WANT TO SHORTEN MORE LINKS?"; echo ""
 echo "Y) YES"
 echo "Q) NO (EXIT)"
-read repeat
-case $repeat in
+read ask2repeat_main
+case $ask2repeat_main in
 	y) clear; main;;
 	Y) clear; main;;
 	q) echo ""; echo "PRESS ANY KEY TO EXIT"; read -n 1 -s; exit;;
 	Q) echo ""; echo "PRESS ANY KEY TO EXIT"; read -n 1 -s; exit;;
-	*) clear; repeat
+	*) clear; ask2repeat_main
 esac
 }
 main
